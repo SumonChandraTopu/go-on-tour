fe8fb5ab4ea6c8f48d7a66a80182ffa460b69f82
Sumon Chandra Shil <topu199041@gmail.com>
2022-05-04 22:13:02 +0600

    Initialize the project

---
+++ src/feature.js/translateApi.js
@@ -0,0 +1,13 @@
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { buildQueries } from "@testing-library/react";
+
+// const headers = {
+//   "content-type": "application/x-www-form-urlencoded",
+//   "Accept-Encoding": "application/gzip",
+//   "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+//   "X-RapidAPI-Key": "2aca2883ccmsha30c691d132f478p12d213jsnd28011583fd9",
+// };
+// const baseUrl =
+//   "https://google-translate1.p.rapidapi.com/language/translate/v2";
+
+// const createRequest = (url) => ({ url, headers });
